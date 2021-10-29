7451c5c9b6ee7fb6b9809050529535ed7ea3d3a1
Felipe Flores <felipefloresvendas@gmail.com>
2021-10-29 08:29:01 -0300

    server runing and showing message

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Funcionando')
+})
+
 const CONNECTION_URL = 'mongodb+srv://felipefloresweb:12345trybe@cluster0.xwqtf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 4000;
 
