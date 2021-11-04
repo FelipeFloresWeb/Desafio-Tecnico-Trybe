3970913307c6ee89eabb5dd0ef4a4f60422c2dcb
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-04 18:49:45 -0300

    Remove environment variable and console logs

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,40 +3,40 @@ import { config } from 'dotenv';
 
 config();
 
-const environmentVariableUrl = process.env.URL || 'http://localhost:4000';
+const URL = 'https://first-todo-list-backend.herokuapp.com';
 
 export const initTask = async (id) => {
-  const fetchApi = await axios.post(`${environmentVariableUrl}/initTask/${id}`);
+  const fetchApi = await axios.post(`${URL}/initTask/${id}`);
 
   return fetchApi;
 };
 
 export const doneTask = async (id) => {
-  const fetchApi = await axios.post(`${environmentVariableUrl}/delete/${id}`);
+  const fetchApi = await axios.post(`${URL}/delete/${id}`);
 
   return fetchApi;
 };
 
 export const addTask = async (data) => {
-  const fetchApi = await axios.post('/addTask', data);
+  const fetchApi = await axios.post(`${URL}/addTask`, data);
 
   return fetchApi;
 };
 
 export const getTasks = async (id) => {
-  const fetchApi = await axios.post(`${environmentVariableUrl}/tasks`, { id });
+  const fetchApi = await axios.post(`${URL}/tasks`, { id });
 
   return fetchApi;
 };
 
 export const createUser = async (data) => {
-  const fetchApi = await axios.post(`${environmentVariableUrl}/create`, data);
+  const fetchApi = await axios.post(`${URL}/create`, data);
 
   return fetchApi;
 };
 
 export const login = async (data) => {
-  const fetchApi = await axios.post(`${environmentVariableUrl}/login`, data);
+  const fetchApi = await axios.post(`${URL}/login`, data);
 
   return fetchApi;
 };
